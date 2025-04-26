1e51a9986e626cc5d44741c7f1821e6df6e8cb49
Luciano Ariel <lucianomartinez85@gmail.com>
2025-04-25 21:55:11 -0300

    comienzo

---
+++ convex/chat.ts
@@ -0,0 +1,50 @@
+
+import { v } from "convex/values";
+
+
+import { query, mutation, internalAction } from "./_generated/server";
+
+export const sendMessage = mutation({
+    args: {
+        user: v.string(),
+        body: v.string(),
+    },
+    handler: async (ctx, args) => {
+        console.log("This TypeScript function is running on the server.");
+        await ctx.db.insert("messages", {
+            user: args.user,
+            body: args.body,
+        });
+    },
+});
+
+
+
+// Add the following function to the file:
+export const getMessages = query({
+    args: {},
+    handler: async (ctx) => {
+        // Get most recent messages first
+        const messages = await ctx.db.query("messages").order("desc").take(50);
+        // Reverse the list so that it's in a chronological order.
+        return messages.reverse();
+    },
+});
+
+
+export const getWikipediaSummary = internalAction({
+    args: { topic: v.string() },
+    handler: async (ctx, args) => {
+        const response = await fetch(
+            "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" +
+            args.topic,
+        );
+
+        return getSummaryFromJSON(await response.json());
+    },
+});
+
+function getSummaryFromJSON(data: any) {
+    const firstPageId = Object.keys(data.query.pages)[0];
+    return data.query.pages[firstPageId].extract;
+}
